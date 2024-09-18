f3c25f91ec8169b7053644067f738cf5a27e3cdc
loeizlmt <lolojardinets@outlook.fr>
2024-09-18 15:28:45 +0200

    remplacement de random(70) par random.randint(1, 70) car on ne peut pas appeler random comme une fonction alors que c'est un module.

--- projet1.py
+++ projet1.py
@@ -1,6 +1,8 @@
 from p5 import *
 import random
 
+
+
 def createCanvas(a,b):
     size(a,b)
     no_stroke()
@@ -10,9 +12,9 @@ def setup():
     r = 20
     bx = 50
     by = 50
-    vx = 23
-    vy = 13
-    createCanvas(1000,500) # crée une fenêtre de 200 x 200 pixels
+    vx = 5
+    vy = 5
+    createCanvas(1000,500) # crée une fenêtre de 1000 x 500 pixels
 def draw():# cette fonction s'exécute  en boucle 60 fois par seconde...
     global bx,by,vx,vy,r
     background(0,0,0)# fond noir
@@ -22,10 +24,13 @@ def draw():# cette fonction s'exécute  en boucle 60 fois par seconde...
     # rebond sur les bords gauche et droit
     if bx < r or bx > 1000 - r:
         vx = -vx
-        fill(random(255),random(255),random(255))
+        bx = random.randint(1, 70)
+        by = random.randint(1, 70)
     if by < r or by > 500 - r: 
         vy = -vy
-        fill(random(255),random(255),random(255))
+        bx = random.randint(1, 70)
+        by = random.randint(1, 70)
     # dessin du cercle
-    circle(bx,by,2*r)    
+    circle(bx,by,2*r)   
+    fill(124,125,255) 
 run()

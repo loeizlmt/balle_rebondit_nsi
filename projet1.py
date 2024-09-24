6c1780115ff08f54005cdd685cb26247bc48c075
loeizlmt <lolojardinets@outlook.fr>
2024-09-24 08:51:15 +0200

    ajout condition d'arrêt

--- projet1.py
+++ projet1.py
@@ -4,6 +4,7 @@ import random
 t = 0
 rebond=False
 rebond1=False
+toto = 0
 
 
 def createCanvas(a,b):# fonction qui crée la fenêtre
@@ -19,7 +20,7 @@ def setup():
     vy = 5
     createCanvas(500,400) # crée une fenêtre de 1000 x 500 pixels
 def draw():# cette fonction s'exécute  en boucle 60 fois par seconde...
-    global bx,by,vx,vy,r,t,rebond1,rebond
+    global bx,by,vx,vy,r,t,rebond1,rebond,toto
     background(0,0,0)# fond noir
     # mouvement du cercle sur l'axe horizontal
     bx = bx + vx
@@ -31,19 +32,26 @@ def draw():# cette fonction s'exécute  en boucle 60 fois par seconde...
         vy = vy +(10/100)
         r = random.randint(1, 10)#changer le rayon du cercle de manière pseudo aléatoire
         rebond=True
+        if r == 10 : 
+            toto = toto + 1
     if by < r and rebond1 == False or by > 400 - r and rebond1 == False: 
         vy = -vy
         vx = vx + (10/100)
         vy = vy + (10/100)
         r = random.randint(1, 10)#changer le rayon du cercle de manière pseudo aléatoire
         rebond1=True
+        if r == 10 : 
+            toto = toto + 1
     if t > 60 :
         rebond=False
         rebond1=False
         t = 0
 
+    if toto == 3 or toto > 3 :
+        exit()
     # dessin du cercle
     circle(bx,by,2*r)   
     fill(124,125,255)
-    t = t + 1 
+    t = t + 1
+    print(toto) 
 run()

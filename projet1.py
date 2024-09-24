dde148929c421b47d65af35aa6c499ec517816de
loeizlmt <lolojardinets@outlook.fr>
2024-09-24 17:16:05 +0200

    ajout de commentaires

--- projet1.py
+++ projet1.py
@@ -3,59 +3,57 @@ import random
 
 toto = 0
 totalRebond = 0
-bxbis = 0
-bybis = 0
 def createCanvas(a,b):# fonction qui crée la fenêtre
     size(a,b)
     no_stroke()
 
 def setup(): 
     global bx,by,vx,vy,r
-    r = random.randint(5,20)#commencer avec le rayon du cercle aléatoirement
+    r = random.randint(5,20)# commencer avec le rayon du cercle aléatoirement
     bx = random.randint(200, 300)# commnce dans un carré de 100px de côté 
     by = random.randint(150, 250)# avec un centre à 250,200
     vx = 4 # variable vitesse horizontale
     vy = 5 # variable vitesse verticale
     createCanvas(500,400) # crée une fenêtre de 500 x 400 pixels
-def changerVitesse() : # fonction
+def changerVitesse() : # fonction qui permet d'augmenter la vitesse verticale et horizontale de la balle
     global vx,vy
     vx = (vx * (10/100)) + vx
     vy = (vy * (10/100)) + vy
-def changerTaille():
+def changerTaille():# fonction qui permet de changer la taille de la balle aléatoirement
     global r
-    r = random.randint(1, 10)#changer le rayon du cercle aléatoirement
+    r = random.randint(1, 10)# changer le rayon du cercle aléatoirement
 
 def draw():# cette fonction s'exécute  en boucle 60 fois par seconde...
-    global bx,by,vx,vy,r,t,rebond1,rebond,toto,totalRebond,bxbis,bybis
+    global bx,by,vx,vy,r,toto,totalRebond
     background(0,0,0)# fond noir
     # mouvement du cercle sur l'axe horizontal
     bx = bx + vx
-    by = by + vy
+    by = by + vy # mouvement du cercle sur l'axe vertical
     # rebond sur les bords gauche et droit
     if bx < r or bx > 500 - r:
-        vx = -vx
-        bxbis = bx
-        changerVitesse()
-        changerTaille()
-        totalRebond = totalRebond +1
+        vx = -vx # inverse la vitesse
+        changerVitesse() # fonction qui augmente la vitesse
+        changerTaille() # fonction qui change la taille
+        totalRebond = totalRebond +1 # permet de compter les rebonds
+        # test si le diamètre est égal à 20 et incrémente la variable toto
         if r == 10 : 
             toto = toto + 1
-        bx = bxbis
+        bx = bx # replace la balle pour éviter que la balle rebondisse à l'infini sur le côté
+    # rebond sur les bords haut et bas
     if by < r or by > 400 - r: 
-        vy = -vy
-        bybis = by
-        changerVitesse()
-        changerTaille()
-        totalRebond = totalRebond +1
+        vy = -vy # inverse la vitesse
+        changerVitesse() # fonction qui augmente la vitesse
+        changerTaille() # fonction qui change la taille
+        totalRebond = totalRebond +1 # permet de compter les rebonds
+        # test si le diamètre est égal à 20 et incrémente la variable toto
         if r == 10 : 
             toto = toto + 1
-        by = bybis
-
+        by = by # replace la balle pour éviter que la balle rebondisse à l'infini sur le côté
+    # vérifie si le diamètre de la balle a été de 20px 3 fois 
     if toto == 3 or toto > 3 :
-        print(f"la balle à rebondit {totalRebond} fois")
-        exit()
+        print(f"la balle à rebondit {totalRebond} fois") # écrit le nombre total de rebond 
+        exit() # arrête le programme
     # dessin du cercle
     circle(bx,by,2*r)   
-    fill(124,125,255)
-    t = t + 1
+    fill(124,125,255) # couleur du cercle
 run()
